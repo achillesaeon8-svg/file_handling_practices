9df0603b2a7adb5659ac659b3a5b02e393f4bdfc
unknown <achillesaeon8-svg>
2026-04-29 21:34:42 +0800

    program 1 class file done

---
+++ program_1_even_and_odd_numbers_partitioner/even_odd_filter.py
@@ -0,0 +1,22 @@
+class OddAndEvenSettings:
+    def __init__(self, source_filename):
+        chosen_file = source_filename
+    
+    def sort_numbers(self):
+        try:
+            with open(chosen_file, 'r') as file:
+                for line in file:
+                    number = int(line.strip())
+
+                    if number % 2 == 0:
+                        open('even.txt', 'w')
+                    else:
+                        open('odd.txt', 'r')
+                    
+            print('Analyzing complete successfully.')
+        except FileNotFoundError:
+            print('Error: File not found')
+        except ValueError:
+            print('Error: The file contains text that is not a number')
+        except Exception as e:
+            print(f'An unexpected error occured: {e}')
