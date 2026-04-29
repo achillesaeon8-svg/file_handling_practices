57673de8e8a13ba3323d718df5524166e983117f
unknown <achillesaeon8-svg>
2026-04-29 22:17:07 +0800

    program 1 class file successfully generated

--- program_1_even_and_odd_numbers_partitioner/main.py
+++ program_1_even_and_odd_numbers_partitioner/main.py
@@ -1,5 +1,5 @@
 from even_odd_filter import OddAndEvenSettings
 
-selected_file = OddAndEvenSettings('number.txt')
+selected_file = OddAndEvenSettings('program_1_even_and_odd_numbers_partitioner/number.txt')
 
 selected_file.sort_numbers()
